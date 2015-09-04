c62190b0f3666c9e078bf997a029f24cf7433313
Christopher David Desjardins <cddesjardins@gmail.com>
2015-09-04 11:57:46 +0000

    first files

---
+++ hello_world.cpp
@@ -0,0 +1,9 @@
+// This program outputs the message "Hello, World!" to the monitor
+
+#include "std_lib_facilities.h"
+
+int main()
+{
+	cout << "Hello, programming!\nHere we go!\n";
+	return 0;
+}
---
+++ input_example.cpp
@@ -0,0 +1,9 @@
+#include "std_lib_facilities.h"
+
+int main(){
+  cout << "Please type your first name followed by the enter key\n";
+  string first_name;
+  cin >> first_name;
+  cout << "Hi " << first_name << ", how are you doing?\n";
+  return 0;
+}
