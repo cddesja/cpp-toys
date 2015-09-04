956d2ec90916a120aed9e9e48a0f92761a069b1d
Christopher David Desjardins <cddesjardins@gmail.com>
2015-09-04 18:39:38 +0000

    one other thought on ordering.

--- ordering.cpp
+++ ordering.cpp
@@ -25,6 +25,45 @@ int main()
   }
 
 /*
+  Option 2: With 3 if statements and 3 more variables
+/*
+int main()
+{
+  int num1;
+  int num2;
+  int num3;
+  int first;
+  int second;
+  int third;
+
+  cout << "Please input 3 numbers seperated by a space: ";
+  cin >> num1 >> num2 >> num3;
+
+  first = num1;
+  second = num2;
+  third = num3;
+
+  if(num2 < num1){
+    second = num1;
+    first = num2;
+  }
+
+  if(num3 < second){
+    third = second;
+    second = num3;
+  }
+
+  if(num3 < first){
+    second = first;
+    first = num3;
+  }
+
+  cout << first << ", " << second << ", " << third << '\n';
+
+}
+
+
+/*
 Order strings
 */
 // int main()
